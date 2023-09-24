a49629bf23e152dd3394b13eab12996b00e5df33
replyre <121796450+replyre@users.noreply.github.com>
2023-09-24 15:25:27 +0530

    added error

--- FoodisWorld/src/pages/Error.jsx
+++ FoodisWorld/src/pages/Error.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="bg-red-200 h-[100vh] flex flex-col justify-center items-center">
+      <p className="text-3xl text-red-600"> Error!</p>
+      <p className="text-xl text-red-700"> Page not found.</p>
+      <button
+        className=" bg-green-500 p-2  rounded-md hover:bg-green-400 text-white mt-4 active:scale-95 min-w-[200px]"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home
+      </button>
+    </div>
+  );
 };
 
 export default Error;
